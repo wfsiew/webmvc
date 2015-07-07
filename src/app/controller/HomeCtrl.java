6690833ef00ca9008ff4bb08328f2b0508bacc5a
wfsiew <siewwingfei@hotmail.com>
2015-07-07 17:31:36 +0800

    a

--- src/app/controller/HomeCtrl.java
+++ src/app/controller/HomeCtrl.java
@@ -6,7 +6,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.sql.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+ 
 
+import javax.net.ssl.HttpsURLConnection;
 import javax.naming.*;
 import javax.sql.*;
 
@@ -14,15 +22,63 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import org.apache.log4j.Logger;
 
 @Controller
 public class HomeCtrl
 {
-	private static Logger logger = Logger.getLogger(HomeCtrl.class);
+	private static Logger logger = Logger.getLogger(HomeCtrl.class);;
+	
+	private final String ds1 = "java:/MSSQLDS";
+	private final String ds2 = "java:/MSSQLDS-120";
+	
+	@RequestMapping("/customers")
+	@ResponseBody
+	public ArrayList<Customer> getCustomers() throws Exception
+	{
+		ArrayList<Customer>  l = new ArrayList<Customer>();
+		PreparedStatement ps = null;
+		ResultSet res = null;
+		Connection con = getConnection(ds2);
+		
+		try
+		{
+			String q = "select top 2000 custid, name from customer";
+			ps = con.prepareStatement(q);
+			res = ps.executeQuery();
+			
+			while (res.next())
+			{
+				Customer o = new Customer();
+				o.setId(res.getInt(1));
+				o.setName(res.getString(2));
+				
+				l.add(o);
+			}
+		}
+		
+		catch (Exception ex)
+		{
+			logger.debug(ex);
+		}
+		
+		finally
+		{
+			if (res != null)
+				res.close();
+			
+			if (ps != null)
+				ps.close();
+			
+			if (con != null)
+				con.close();
+		}
+		
+		return l;
+	}
 	
 	@RequestMapping("/users")
 	@ResponseBody
@@ -31,7 +87,7 @@ public class HomeCtrl
 		ArrayList<User> l = new ArrayList<User>();
 		PreparedStatement ps = null;
 		ResultSet res = null;
-		Connection con = getConnection();
+		Connection con = getConnection(ds1);
 		
 		try
 		{
@@ -101,9 +157,53 @@ public class HomeCtrl
 		return m;
 	}
 	
-	private Connection getConnection()
+	@RequestMapping("/callback")
+	public Map<String, Object> callback(@RequestParam("code") String code, 
+			                            @RequestParam("session_state") String sessionState) throws IOException
 	{
-		String dscontext = "java:/MSSQLDS";
+		Map<String, Object> m = new HashMap<String, Object>();
+		m.put("success", 1);
+		
+		String url = "https://www.googleapis.com/oauth2/v3/token";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		
+		con.setRequestMethod("POST");
+		
+		String urlParameters = "code=" + code + "&client_id=67995890535.apps.googleusercontent.com&client_secret=MTnxddiDsRnuLJY6Rp9uCoo7&redirect_uri=http://localhost:8080/webmvc/callback2&grant_type=authorization_code";
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+		
+		int responseCode = con.getResponseCode();
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		m.put("response", response.toString());
+		
+		return m;
+	}
+	
+	@RequestMapping("/callback2")
+	@ResponseBody
+	public String callback2()
+	{
+		return "success";
+	}
+	
+	private Connection getConnection(String ds)
+	{
+		String dscontext = ds;
 		
 		Connection con = null;
 		
---
+++ web/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>Index</h1>
+<a href="https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=767995890535.apps.googleusercontent.com&redirect_uri=http://localhost:8080/webmvc/callback&scope=openid%20profile%20email&login_hint=email">Login</a>
+</body>
+</html>
